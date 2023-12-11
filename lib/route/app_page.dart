bcab07682edec1b44b975d7f536c7611258b5b12
kazungudev <kazungudev@gmail.com>
2023-12-11 19:10:36 +0300

    full complete commit

---
+++ lib/route/app_page.dart
@@ -0,0 +1,14 @@
+import 'package:get/get.dart';
+import 'package:e_bay/route/app_route.dart';
+import 'package:e_bay/view/dashboard/dashboard_binding.dart';
+import 'package:e_bay/view/dashboard/dashboard_screen.dart';
+
+class AppPage {
+  static var list = [
+    GetPage(
+        name: AppRoute.dashboard,
+        page: () => const DashboardScreen(),
+        binding: DashboardBinding()
+    ),
+  ];
+}
